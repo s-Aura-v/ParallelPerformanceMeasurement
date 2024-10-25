77f3ad3c44e651b836aa1526ba0c5cce03c5904e
s-Aura-v <slamich2@oswego.edu>
2024-10-24 23:02:36 -0400

    Initialized Spring

---
+++ src/main/java/com/performance_measurement/perforamancemeasurement/PerforamancemeasurementApplication.java
@@ -0,0 +1,13 @@
+package com.performance_measurement.perforamancemeasurement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerforamancemeasurementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerforamancemeasurementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/performance_measurement/perforamancemeasurement/PerforamancemeasurementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.performance_measurement.perforamancemeasurement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerforamancemeasurementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
