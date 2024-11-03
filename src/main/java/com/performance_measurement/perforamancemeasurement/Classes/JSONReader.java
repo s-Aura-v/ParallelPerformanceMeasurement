d20bbc9b99f4c84b34a720185c5aa1282d05e7cb
s-Aura-v <slamich2@oswego.edu>
2024-11-02 20:35:32 -0400

    ExecutorService Implemented

--- src/main/java/com/performance_measurement/perforamancemeasurement/Classes/JSONReader.java
+++ src/main/java/com/performance_measurement/perforamancemeasurement/Classes/JSONReader.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class JSONReader {
 
@@ -19,17 +20,26 @@ public class JSONReader {
             skipList.insert(isbnID);
         }
 
-        System.out.println(skipList.search(9780679732761L));
-        System.out.println(skipList.search(2));
-        skipList.delete(9780679732761L);
-        skipList.insert(2);
-        System.out.println(skipList.search(9780679732761L));
-        System.out.println(skipList.search(2));
         return skipList;
     }
 
     // tester for json reader
     public static void main(String[] args) throws IOException {
         File jsonFile = new File("src/main/resources/database/short_BookList.json");
+        SkipList skipList = convertJSON(jsonFile);
+
+
+        System.out.println(skipList.search(9780679732761L));
+        System.out.println(skipList.search(2));
+        skipList.delete(9780679732761L);
+        skipList.insert(2);
+        System.out.println(skipList.search(9780679732761L));
+        System.out.println(skipList.search(2));
+
+
+        int readProbability = ThreadLocalRandom.current().nextInt(100);
+        int randomNumber = ThreadLocalRandom.current().nextInt(0, 1) * readProbability;
+
+
     }
 }
--- src/main/java/com/performance_measurement/perforamancemeasurement/Classes/Librarian.java
+++ src/main/java/com/performance_measurement/perforamancemeasurement/Classes/Librarian.java
@@ -3,23 +3,49 @@ package com.performance_measurement.perforamancemeasurement.Classes;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Librarian implements Runnable {
-    private int numberOfLibrarians;
-    private int readProbability;
+    private Library library;
 
-    public Librarian(int numberOfLibrarians, int readProbability) {
+    //DEBUG
+    int writeCount = 0;
+    int readCount = 0;
+
+    private int numberOfLibrarians;
+    // ReadProbability = A number between 0 and 1.
+    private final double readProbability;
+
+    public Librarian(int numberOfLibrarians, double readProbability) {
         this.numberOfLibrarians = numberOfLibrarians;
         this.readProbability = readProbability;
+
+//        //DEBUG
+//        this.readProbability = .95;
     }
 
     @Override
     public void run() {
-        int randomNumber = ThreadLocalRandom.current().nextInt(0, 100) * readProbability;
-        if (randomNumber < readProbability) {
-            // WRITE
+        for (int i = 0; i < numberOfLibrarians; i++) {
+            runSkipListCalculation();
+        }
+        System.out.println(Thread.currentThread() + ": Read   " + readCount + "   Write: " + writeCount);
+    }
 
-        } else {
+    private void initializeHo() {
+
+    }
+
+    private void runSkipListCalculation() {
+        int randomNumber = ThreadLocalRandom.current().nextInt(0, 100);
+        if (randomNumber < readProbability * 100) {
             // READ
-
+            readCount++;
+        } else {
+            // WRITE
+            writeCount++;
         }
     }
+
+    private void runJavaUtilityCalculation() {
+
+    }
+
 }
